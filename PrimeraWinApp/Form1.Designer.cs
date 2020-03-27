2807a5832fdd4a27b39794fa263facf128ae8647
Maximiliano Sar Fernández <msarfernandez@MacBook-Pro-de-Maximiliano.local>
2020-03-27 20:44:01 -0300

    hola que tal

---
+++ PrimeraWinApp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PrimeraWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnPreguntar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(313, 194);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(237, 103);
+            this.btnSaludar.TabIndex = 0;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(241, 110);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(390, 31);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(263, 378);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(70, 25);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "label1";
+            // 
+            // btnPreguntar
+            // 
+            this.btnPreguntar.Location = new System.Drawing.Point(649, 194);
+            this.btnPreguntar.Name = "btnPreguntar";
+            this.btnPreguntar.Size = new System.Drawing.Size(153, 105);
+            this.btnPreguntar.TabIndex = 3;
+            this.btnPreguntar.Text = "Salir";
+            this.btnPreguntar.UseVisualStyleBackColor = true;
+            this.btnPreguntar.Click += new System.EventHandler(this.btnPreguntar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 506);
+            this.Controls.Add(this.btnPreguntar);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnSaludar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Primera Aplicacion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnPreguntar;
+    }
+}
+
---
+++ PrimeraWinApp/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeraWinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            //esto se va a ejecutar cuando hagas click en el boton Saludar.
+            if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Debes ingresar tu nomnbre");
+            }
+            else
+            {
+                MessageBox.Show("Hola " + txtNombre.Text);
+            }
+            Form1_Load(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Te damos la bienvenida a la super app", "HOLA!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            lblNombre.Text = txtNombre.Text;
+            
+        }
+
+        private void btnPreguntar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("De verdad queres salir?", "Salir", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Dispose();
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
